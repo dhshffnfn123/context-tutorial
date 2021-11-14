3479a1bfa9d188893602dfba40d2727784bab580
B H An <anbyunghoon0107@gamil.com>
2021-11-14 20:56:21 +0900

    동적 컨텍스트 전

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 > + Consumer 사이에 중괄호를 열어서 그 안에 함수를 넣어 주는 패턴을 Funtion as a child, 혹은 Render Props라고 한다.
 > - 컴포넌트의 children이 있어야 할 자리에 일반 JSX 혹은 문자열이 아닌 함수를 전달하는 것이다.
 > * color.js와 ColorBox.js 참고
+
+## Provider
+> + provider를 사용하면 Context의 value를 변경할 수 있다.
+> - provider를 사용할 때는 value 값을 명시해 주어야 제대로 작동한다.
+
+## 동적 Context 파일 수정하기
+> + Context의 value에는 상태 값만이 아닌 함수를 전달해 줄 수 있다.
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 const App = () => {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    <ColorContext.Provider value={{ color: 'red' }}> {/* value를 설정하지 않으면 오류가 난다. */}
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 };
 
